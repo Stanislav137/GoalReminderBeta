454d89fc0b6832ff4670315ee698b61592fcbed4
Stason Stas <semenetsbeph@gmail.com>
2017-07-11 21:36:39 +0200

    added animation for all circle button

--- app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
+++ app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
@@ -1,16 +1,35 @@
 package com.goalreminderbeta.sa.goalreminderbeta;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button myGoal, myRecords, myProgress, myOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myGoal = (Button) findViewById(R.id.myGoal);        // ToDO через коллекцию вроде как не получится (если я еще не совсем даун)
+        myRecords = (Button) findViewById(R.id.myRecords);
+        myProgress = (Button) findViewById(R.id.myProgress);
+        myOptions = (Button) findViewById(R.id.myOptions);
+
+        Animation animCircleGreen = AnimationUtils.loadAnimation(this, R.anim.btn_green); // ToDo может как-то можно в листу общую добавить, хотя нету смысла. Или метод отдельный сделать
+        Animation animCircleRed = AnimationUtils.loadAnimation(this, R.anim.btn_red);
+        Animation animCircleBlue = AnimationUtils.loadAnimation(this, R.anim.btn_blue);
+        Animation animCircleOrange = AnimationUtils.loadAnimation(this, R.anim.btn_orange);
+        myGoal.startAnimation(animCircleGreen);
+        myRecords.startAnimation(animCircleRed);
+        myProgress.startAnimation(animCircleOrange);
+        myOptions.startAnimation(animCircleBlue);
     }
 }
