e7cec5891e63a604d3a0166b3cdb54d538317222
Stason Stas <semenetsbeph@gmail.com>
2017-07-15 19:15:39 +0200

    create main page (menu added) and 3 frgm.

--- app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
+++ app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
@@ -6,30 +6,20 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-
-    Button myGoal, myRecords, myProgress, myOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animAddGoal();
 
-        myGoal = (Button) findViewById(R.id.myGoal);        // ToDO через коллекцию вроде как не получится (если я еще не совсем даун)
-        myRecords = (Button) findViewById(R.id.myRecords);
-        myProgress = (Button) findViewById(R.id.myProgress);
-        myOptions = (Button) findViewById(R.id.myOptions);
-
-        Animation animCircleGreen = AnimationUtils.loadAnimation(this, R.anim.btn_green); // ToDo может как-то можно в листу общую добавить, хотя нету смысла. Или метод отдельный сделать
-        Animation animCircleRed = AnimationUtils.loadAnimation(this, R.anim.btn_red);
-        Animation animCircleBlue = AnimationUtils.loadAnimation(this, R.anim.btn_blue);
-        Animation animCircleOrange = AnimationUtils.loadAnimation(this, R.anim.btn_orange);
-        myGoal.startAnimation(animCircleGreen);
-        myRecords.startAnimation(animCircleRed);
-        myProgress.startAnimation(animCircleOrange);
-        myOptions.startAnimation(animCircleBlue);
     }
+
+    public void animAddGoal() {
+        Button spaceshipImage = (Button) findViewById(R.id.myGoal);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.btn_green);
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+    }
+
 }
