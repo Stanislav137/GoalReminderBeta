2078d9ff4120155055ebdffab3d700b497fff165
Stason Stas <semenetsbeph@gmail.com>
2017-08-23 21:08:40 +0200

    new commit added expanders

--- app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
+++ app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,13 +23,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.habbits_theme);
-//        animCircleAddGoal();
-//
-//        frgmGoals = new FrgmGoals();
-//        frgmRecords = new FrgmRecords();
-//        frgmOptions = new FrgmOptions();
-//
+        setContentView(R.layout.activity_main);
+       // animCircleAddGoal();
+
+        ExpandableListView listView = (ExpandableListView)findViewById(R.id.exListView);
+
+        ArrayList<ArrayList<String>> groups = new ArrayList<>();
+        ArrayList<String> children1 = new ArrayList<>();
+        ArrayList<String> children2 = new ArrayList<>();
+        children1.add("Child_2");
+        children2.add("Child_2");
+        children2.add("Child_2");
+        children2.add("Child_3");
+        groups.add(children1);
+        groups.add(children2);
+
+        ExpListAdapter adapter = new ExpListAdapter(getApplicationContext(), groups);
+        listView.setAdapter(adapter);
+
+        frgmGoals = new FrgmGoals();
+        frgmRecords = new FrgmRecords();
+        frgmOptions = new FrgmOptions();
+
 //        rlGoalsBtn = (RelativeLayout) findViewById(R.id.rlGoalBtn);
 //        rlRecordsBtn = (RelativeLayout) findViewById(R.id.rlRecordsBtn);
 //        rlOptionsBtn = (RelativeLayout) findViewById(R.id.rlOptionsBtn);
@@ -40,20 +58,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClick(View v) {
-        fTrans = getFragmentManager().beginTransaction();
-        switch (v.getId()) {
-            case R.id.rlGoalBtn:
-                fTrans.add(R.id.frgmCont, frgmGoals);
-                break;
-            case R.id.rlRecordsBtn:
-                fTrans.replace(R.id.frgmCont, frgmRecords);
-                break;
-            case R.id.rlOptionsBtn:
-                fTrans.replace(R.id.frgmCont, frgmOptions);
-            default:
-                break;
-        }
-        fTrans.commit();
+//        fTrans = getFragmentManager().beginTransaction();
+//        switch (v.getId()) {
+//            case R.id.rlGoalBtn:
+//                fTrans.add(R.id.frgmCont, frgmGoals);
+//                break;
+//            case R.id.rlRecordsBtn:
+//                fTrans.replace(R.id.frgmCont, frgmRecords);
+//                break;
+//            case R.id.rlOptionsBtn:
+//                fTrans.replace(R.id.frgmCont, frgmOptions);
+//            default:
+//                break;
+//        }
+//        fTrans.commit();
     }
 
     public void animCircleAddGoal() {
