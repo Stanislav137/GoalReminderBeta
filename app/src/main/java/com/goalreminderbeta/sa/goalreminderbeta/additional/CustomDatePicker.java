ac0e7b0f83e269225674d1f5bb98bf7274b34b06
Otrotskyi <anton.otrotskyi@gmail.com>
2017-09-16 13:47:10 +0200

    Added CustomDatePicker

---
+++ app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/additional/CustomDatePicker.java
@@ -0,0 +1,37 @@
+package com.goalreminderbeta.sa.goalreminderbeta.additional;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CustomDatePicker {
+
+    public static void pickDate(Activity activity, final TextView view){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(activity, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(android.widget.DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                String stringDate = day+"/"+month+"/"+year;
+                Date date = null;
+                try {
+                    date = formatter.parse(stringDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                view.setText(formatter.format(date));
+            }
+        }, year, month, day);
+        dialog.show();
+    }
+}
