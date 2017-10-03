5c3548603c59132be0c52a5819216811bc9cdec4
Anton Otrotskyi <anton.otrotskyi@gmail.com>
2017-10-03 20:00:35 +0200

    Implemented notifications for testing

--- app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/all/science/AllSubThemesScience.java
+++ app/src/main/java/com/goalreminderbeta/sa/goalreminderbeta/all/science/AllSubThemesScience.java
@@ -1,24 +1,38 @@
 package com.goalreminderbeta.sa.goalreminderbeta.all.science;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.goalreminderbeta.sa.goalreminderbeta.R;
 import com.goalreminderbeta.sa.goalreminderbeta.additional.BootStrap;
+import com.goalreminderbeta.sa.goalreminderbeta.all.OptionsActivity;
+import com.goalreminderbeta.sa.goalreminderbeta.all.StartActivity;
+import com.goalreminderbeta.sa.goalreminderbeta.goals.WeightCorrectionGoal;
+
 import java.util.ArrayList;
 
 public class AllSubThemesScience extends AppCompatActivity {
 
     private LinearLayout subWeight;
+    private NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_subthemes_science);
 
+        notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         subWeight = (LinearLayout) findViewById(R.id.subWeight);
         findImagesBanners();
     }
